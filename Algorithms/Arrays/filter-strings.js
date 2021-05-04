7b39d4df45aa7e20de7543e907aebe276a739379
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-05-04 18:12:54 -0400

    new array algo

---
+++ Algorithms/Arrays/filter-strings.js
@@ -0,0 +1,7 @@
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+// If a name has exactly 4 letters in it, they are your friend, otherwise they are not. 
+
+
+function friend(friends) {
+  return myFriends = friends.filter(friend => friend.length === 4)
+}
